2b085ccaab98e4b37529ba9332d0397d09dad82d
lalitsawantt <llaalliitt251@gmail.com>
2021-01-22 03:09:12 +0530

    changed Zero matrix

--- Zero matrix/src/com/company/Main.java
+++ Zero matrix/src/com/company/Main.java
@@ -6,83 +6,85 @@ import java.util.Scanner;
 public class Main {
 //Given a matrix MxN, if any element is 0 then the whole row and column are set to 0
     public static void main(String[] args) {
-	// write your code here
         Scanner sc = new Scanner(System.in);
-        int rows, column;
-        System.out.println("Enter the number of rows");
+        int rows, columns;
+        System.out.println("Enter the number of rows ");
         rows = sc.nextInt();
-        System.out.println("Enter the number of columns");
-        column = sc.nextInt();
+        System.out.println("Enter the number of columns ");
+        columns = sc.nextInt();
+        int matrix[][] = new int[rows][columns];
+        int rowHasZero = -1, colHasZero = -1;
         System.out.println("Enter the matrix elements");
-        int matrix[][] = new int[rows][column];
         for(int i = 0; i < rows; i++){
-            for(int j = 0; j < column; j++){
+            for(int j = 0; j < columns; j++){
                 matrix[i][j] = sc.nextInt();
             }
         }
-        int rowHasZero=-1, columnHasZero=-1;
-//        check if the first row or column has zeroes
-        for(int i=0; i<column;i++){
-            if(matrix[0][i] == 0){
-                rowHasZero = i;
-            }
-        }
-        for(int i=0; i<rows; i++){
-            if(matrix[i][0] == 0){
-                columnHasZero = i;
-            }
-        }
-//        check if the rest of the matrix has zeroes in any position
-//        if yes, then make the corresponding first element in that row
-//        and column = 0
-//        then parse the first row and column and nullify those which have zeroes
-        for(int i = 1; i < rows; i++){
-            for(int j = 1; j < column; j++){
-                if(matrix[i][j] == 0){
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
-            }
-        }
-//        call nullifyRow and nullify column for every element that is zero in the first row and column
+        System.out.println("original matrix");
         for(int i = 0; i < rows; i++){
-            if(matrix[i][0]==0){
-
-                nullifyRow(matrix,i);
+            for(int j = 0; j < columns; j++){
+                System.out.print(matrix[i][j] + "  ");
             }
+            System.out.println("");
         }
-        for(int i = 0; i < column; i++){
-            if(matrix[0][i] == 0){
-                nullifyCol(matrix,i);
-
-            }
-        }
-
-        if(rowHasZero!=-1){
-            nullifyRow(matrix, 0);
-            nullifyCol(matrix,rowHasZero);
-        }
-        if(columnHasZero!=-1){
-            nullifyRow(matrix, columnHasZero);
-            nullifyCol(matrix,0);
-        }
-
-        for(int i=0; i < rows; i++){
-            for(int j=0; j < column; j++){
+        setZeroes(matrix);
+        System.out.println("output matrix");
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
                 System.out.print(matrix[i][j] + "  ");
             }
             System.out.println("");
         }
     }
 
+    static void setZeroes(int [][] matrix){
+        boolean rowHasZero = false;
+        boolean colHasZero = false;
+        
+        for(int j = 0; j < matrix[0].length; j++){
+            if(matrix[0][j] == 0){
+                rowHasZero = true;
+                break;
+            }
+        }
+        for(int i = 0; i < matrix.length; i++){
+            if(matrix[i][0] == 0){
+                colHasZero = true;
+                break;
+            }
+        }
+
+//        check for zeroes in the rest of the matrix
+        for(int i = 1; i < matrix.length; i++){
+            for(int j = 1; j < matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < matrix.length; i++){
+            if(matrix[i][0] == 0){
+                nullifyRow(matrix,i);
+            }
+        }
+        for(int j = 1; j < matrix[0].length; j++){
+            if(matrix[0][j] == 0){
+                nullifyCol(matrix,j);
+            }
+        }
+        if(rowHasZero) nullifyRow(matrix, 0);
+        if(colHasZero) nullifyCol(matrix, 0);
+    }
     static void nullifyRow(int[][] arr, int row){
-        for(int i = 0; i < arr.length; i++){
+        for(int i = 0; i < arr[0].length; i++){
             arr[row][i] = 0;
         }
     }
 
-    static void nullifyCol(int[][] arr, int col){
-        for(int i = 0; i < arr[0].length; i++){
+    static void nullifyCol(int[][] arr, int col) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i][col] = 0;
         }
     }
